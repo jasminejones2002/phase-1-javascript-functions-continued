169a2f3d11dfb3eb134ab7c0efb4048f80ada28d
jasminejones2002 <jazziejones02@yahoo.com>
2023-12-13 15:33:10 -0600

    functions continued lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+mondayWork();
+
+function wrapAdjective(param = '*') {
+    let innerFunction = function (adj = 'special') {
+        return `You are ${param}${adj}${param}!`
+    }
+    return innerFunction;
+}
+wrapAdjective(param)(adj);
